52955265bc28d21dbfccac43ac087796caee5f10
EvgeniiK <kachaloff89@gmail.com>
2023-06-16 21:01:34 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по 
+// убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToInt32(new Random().Next(1, 10));
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j],5}");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void DecreaseLines(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+Console.Clear();
+System.Console.WriteLine("Введите количество строк");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine("----------------------");
+DecreaseLines(array);
+PrintArray(array);
